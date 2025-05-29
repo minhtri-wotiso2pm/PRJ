0b317efeb671d6decd199ffbfcef2f260702e25e
zanh154 <nzero1542005@gmail.com>
2025-05-29 22:07:11 +0700

    Update java.js

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ window.onscroll = function () {
     var font = document.querySelectorAll(".font-img a, .menu a");
     var button = document.querySelectorAll(".login,.login-wel");
     var logo = document.getElementById("logo-loka");
-    var user = document.getElementById("user-logo-wel") || document.getElementById("user-logo-login");
+    var user = document.getElementById("user-logo-login")||document.getElementById("user-logo-wel");
 
     let isScrolled = window.scrollY > 0;
 
@@ -26,7 +26,54 @@ window.onscroll = function () {
     }
 };
 
-// Gọi ngay sau khi trang load
+function initBookingMenu() {
+    const bookingItems = document.querySelectorAll(".booking-item");
+    
+    for(let i=0; i<bookingItems.length; i++){
+        bookingItems[i].addEventListener('click', function() {
+            const activeItem = document.querySelector('.booking-item.active');
+            
+            for(let j=0; j<bookingItems.length; j++){
+                bookingItems[j].classList.remove('active');
+                const img = bookingItems[j].querySelector('img');
+                if(img.id==='hotel'){
+                    img.src = "assets/images/logo-hotel.png";
+                }else if(img.id==='maybay'){
+                    img.src = "assets/images/maybay.png";
+                }else{
+                    img.src = "assets/images/bus-removebg-preview.png";
+                }
+            }
+
+            this.classList.add('active');
+            
+            const activeImg = this.querySelector('img');
+            if(activeImg.id==='hotel'){
+                activeImg.src = "assets/images/lgoo-hotelclick.png";
+            }else if(activeImg.id==='maybay'){
+                activeImg.src = "assets/images/maybayclick.png";
+            }else{
+                activeImg.src = "assets/images/busclick.png";
+            }
+        });
+    }
+}
+
+function initBookingHotel() {
+    const hotelItems = document.querySelectorAll(".hotelcs");
+
+    hotelItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Bỏ class active-hotel khỏi tất cả
+            hotelItems.forEach(i => i.classList.remove('active-hotel'));
+            // Thêm class active-hotel vào phần tử được click
+            this.classList.add('active-hotel');
+        });
+    });
+}
+
 window.onload = function () {
     window.onscroll();
+    initBookingMenu(); 
+    initBookingHotel();
 };
