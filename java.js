ee956f746f238dd040dc800951761d7d2218e9cd
zanh154 <nzero1542005@gmail.com>
2025-05-29 07:16:20 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,24 +1,32 @@
-window.onscroll = function(){
+window.onscroll = function () {
     var box = document.getElementById("box");
-    var font = document.querySelectorAll(".font-img p,.menu a");
-    var button = document.querySelector(".login");
+    var font = document.querySelectorAll(".font-img a, .menu a");
+    var button = document.querySelectorAll(".login,.login-wel");
     var logo = document.getElementById("logo-loka");
-    var user = document.getElementById("user-logo");
-    if(window.scrollY>0){
+    var user = document.getElementById("user-logo-wel") || document.getElementById("user-logo-login");
+
+    let isScrolled = window.scrollY > 0;
+
+    if (isScrolled) {
         box.classList.add("white-bg");
-        button.classList.add("white-bg");
+        button.forEach(btn => btn.classList.add("white-bg"));
         logo.src = "assets/images/d4969644-5d07-445a-b9ba-b38d158b34ce-removebg-preview.png";
         user.src = "assets/images/user-blue.png";
-        for(var i=0;i<font.length;i++){
-            font[i].style.color = "black" 
-        }
-    }else{
+    } else {
         box.classList.remove("white-bg");
-        button.classList.remove("white-bg");
+        button.forEach(btn => btn.classList.remove("white-bg"));
         logo.src = "assets/images/logo_white.png";
         user.src = "assets/images/user-white.png";
-        for(var i=0;i<font.length;i++){
-            font[i].style.color = "white" 
-        }
     }
+
+    for (let i = 0; i < font.length; i++) {
+        font[i].style.color = isScrolled ? "black" : "white";
+        font[i].classList.remove("hover-black", "hover-white");
+        font[i].classList.add(isScrolled ? "hover-white" : "hover-black");
+    }
+};
+
+// Gọi ngay sau khi trang load
+window.onload = function () {
+    window.onscroll();
 };
